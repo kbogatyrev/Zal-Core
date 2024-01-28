db7148aa3b8fb4689d4688da3123113cc0aba3dc
Konstantin <kbogatyrev@yahoo.com>
2024-01-27 23:34:12 -0800

    Progress checkin.

--- RunPhonotactics/RunPhonotactics.cpp
+++ RunPhonotactics/RunPhonotactics.cpp
@@ -19,8 +19,19 @@ ET_ReturnCode CPhonotactics::eInit(const CEString& sDbPath)
 {
     auto pSingleton = Hlib::Singleton::pGetInstance();
     shared_ptr<CDictionary> spDictionary;
-    [[maybe_unused]] auto rc = pSingleton->eGetDictionary(spDictionary);
+    auto rc = pSingleton->eGetDictionary(spDictionary);
+    if (rc != H_NO_ERROR)
+    {
+        ERROR_LOG(L"Failed to set DB path.");
+        return rc;
+    }
+
     rc = spDictionary->eSetDbPath(sDbPath);
+    if (rc != H_NO_ERROR)
+    {
+        ERROR_LOG(L"Failed to set DB path.");
+        return rc;
+    }
 
     m_sCurrentSequence.SetBreakChars(L" ");
 
@@ -35,9 +46,14 @@ ET_ReturnCode CPhonotactics::eCountSoundPairs()
     {
         if (llCurrentTextId != llTextId)
         {
-            if (!m_mapSoundPairToCount.empty())
+            if (!m_mapSoundPairIdToCount.empty())
             {
                 auto rc = eSaveTextData(llCurrentTextId);
+                if (rc != H_NO_ERROR)
+                {
+                    return rc;
+                }
+                m_mapSoundPairIdToCount.clear();
             }
         }
         auto rc = eLoadSegment(llLineId);
@@ -74,14 +90,25 @@ ET_ReturnCode CPhonotactics::eCountSoundPairs()
                 return rc;
             }
 
-            auto pairSounds = make_pair(eLhs, eRhs);
-            if (m_mapSoundPairToCount.count(pairSounds) < 1) 
+            if (m_mapSoundPairToId.count(make_pair(eLhs, eRhs)) < 1) 
             {
-                m_mapSoundPairToCount[pairSounds] = 1;
+                int64_t llSoundPairId{-1};
+                auto rc = eRegisterSoundPair(eLhs, eRhs, llSoundPairId);
+                if (rc != H_NO_ERROR)
+                {
+                    return rc;
+                }
+                m_mapSoundPairToId[make_pair(eLhs, eRhs)] = llSoundPairId;
+            }
+
+            auto llSoundPairId = m_mapSoundPairToId[make_pair(eLhs, eRhs)];
+            if (m_mapSoundPairIdToCount.count(llSoundPairId) < 1) 
+            {
+                m_mapSoundPairIdToCount[llSoundPairId] = 1;
             }
             else
             {
-                m_mapSoundPairToCount[pairSounds] = ++m_mapSoundPairToCount[pairSounds];
+                m_mapSoundPairIdToCount[llSoundPairId] = ++m_mapSoundPairIdToCount[llSoundPairId];
             }
         }
     }
@@ -90,14 +117,51 @@ ET_ReturnCode CPhonotactics::eCountSoundPairs()
 }       //  eCountSoundPairs()
 
 //  -------------------------------------------------------------------------
+ET_ReturnCode CPhonotactics::eLoadPhonemicPairs()
+{
+    CEString sQuery(L"SELECT id, lhs_phoneme, rhs_phoneme FROM phonemic_pairs;");
+    try
+    {
+        int64_t llPairId{-1};
+        m_spDb->PrepareForSelect(sQuery);
+        if (m_spDb->bGetRow())
+        {
+            m_spDb->GetData(0, llPairId);
+            int i1stSound{0};
+            m_spDb->GetData(1, i1stSound);
+            if (i1stSound < (int)ET_Sound::SOUND_UNDEFINED
+                || i1stSound >(int)ET_Sound::SOUND_COUNT)
+            {
+                ERROR_LOG(L"Unrecognized left ET_Sound value.");
+                return H_ERROR_UNEXPECTED;
+            }
+            int i2ndSound{0};
+            m_spDb->GetData(2, i2ndSound);
+            if (i1stSound < (int)ET_Sound::SOUND_UNDEFINED
+                || i1stSound >(int)ET_Sound::SOUND_COUNT)
+            {
+                ERROR_LOG(L"Unrecognized right ET_Sound value.");
+                return H_ERROR_UNEXPECTED;
+            }
+            m_mapSoundPairToId[make_pair((ET_Sound)i1stSound, (ET_Sound)i2ndSound)] = llPairId;
+        }
+        m_spDb->Finalize();
+    }
+    catch (CException& e)
+    {
+        CEString sMsg;
+        eHandleDbException(move(e), sMsg);
+        return H_ERROR_DB;
+    }
+
+    return H_NO_ERROR;
+}
 
 ET_ReturnCode CPhonotactics::eLoadLineIds()
 {
-    CEString sQuery = 
-        L"SELECT DISTINCT text_id, line_id FROM lines_in_text as lit \
-          INNER JOIN tact_group as tg ON tg.line_id=lit.id \
-          ORDER BY text_id, line_id;";
-
+    CEString sQuery (L"SELECT DISTINCT text_id, line_id FROM lines_in_text as lit \
+                       INNER JOIN tact_group as tg ON tg.line_id=lit.id \
+                       ORDER BY text_id, line_id;");
     try
     {
         m_spDb->PrepareForSelect(sQuery);
@@ -152,8 +216,64 @@ ET_ReturnCode CPhonotactics::eLoadSegment(int64_t iSegmentId)
 
 }       //  eLoadSegment()
 
+ET_ReturnCode CPhonotactics::eRegisterSoundPair(ET_Sound eLhs, ET_Sound eRhs, int64_t& llId)
+{
+    try
+    {
+        sqlite3_stmt* pStmt = nullptr;
+        m_spDb->uiPrepareForInsert(L"phonemic_pairs", 2, pStmt);
+        auto llInsertHandle = (unsigned long long)pStmt;
+        m_spDb->Bind(1, eLhs, llInsertHandle);
+        m_spDb->Bind(2, eRhs, llInsertHandle);
+        m_spDb->InsertRow(llInsertHandle);
+        llId = m_spDb->llGetLastKey();
+        m_spDb->Finalize(llInsertHandle);
+    }
+    catch (CException& e)
+    {
+        CEString sMsg;
+        eHandleDbException(move(e), sMsg);
+        return H_ERROR_DB;
+    }
+    return H_NO_ERROR;
+}
+
 ET_ReturnCode CPhonotactics::eSaveTextData(int64_t llTextId)
 {
+    CEString sDeleteQuery(L"DELETE FROM phonemic_counts WHERE text_id = #;");
+    sDeleteQuery = sDeleteQuery.sReplace(L"#", CEString::sToString(llTextId));
+    try
+    {
+        m_spDb->Delete(sDeleteQuery);
+    }
+    catch (CException& ex)
+    {
+        CEString sMsg;
+        eHandleDbException(move(ex), sMsg);
+        return H_ERROR_DB;
+    }
+
+    for (auto pairIdToCount : m_mapSoundPairIdToCount)
+    {
+        try
+        {
+            sqlite3_stmt* pStmt = nullptr;
+            m_spDb->uiPrepareForInsert(L"phonemic_counts", 3, pStmt);
+            auto llInsertHandle = (unsigned long long)pStmt;
+            m_spDb->Bind(1, llTextId, llInsertHandle);
+            m_spDb->Bind(2, pairIdToCount.first, llInsertHandle);
+            m_spDb->Bind(3, pairIdToCount.second, llInsertHandle);
+            m_spDb->InsertRow(llInsertHandle);
+            m_spDb->Finalize(llInsertHandle);
+        }
+        catch (CException& e)
+        {
+            CEString sMsg;
+            eHandleDbException(move(e), sMsg);
+            return H_ERROR_DB;
+        }
+        return H_NO_ERROR;
+    }
     return H_NO_ERROR;
 }
 
--- RunPhonotactics/RunPhonotactics.h
+++ RunPhonotactics/RunPhonotactics.h
@@ -3,10 +3,12 @@
 
 #include <map>
 #include <vector>
+#include <set>
+#include <tuple>
 
 #include "Enums.h"
 #include "EString.h"
-#include "SQLiteWrapper.h"
+#include "SqliteWrapper.h"
 #include "Singleton.h"
 #include "Dictionary.h"
 
@@ -19,8 +21,10 @@ namespace Hlib
         ET_ReturnCode eCountSoundPairs();
 
     private:
+        ET_ReturnCode eLoadPhonemicPairs();
         ET_ReturnCode eLoadLineIds();
         ET_ReturnCode eLoadSegment(int64_t llLineId);
+        ET_ReturnCode eRegisterSoundPair(ET_Sound eLhs, ET_Sound eRhs, int64_t& llId);
         ET_ReturnCode eSaveTextData(int64_t llTextId);
         ET_ReturnCode eStringToSound(CEString&&, ET_Sound&);
         ET_ReturnCode eHandleDbException(CException&&, CEString& sMsg);
@@ -28,7 +32,9 @@ namespace Hlib
     private:
         shared_ptr<CSqlite> m_spDb;
         multimap<int64_t, int64_t> m_mmapTextIdToLineIds;
-        map<pair<ET_Sound, ET_Sound>, int64_t> m_mapSoundPairToCount;
+        map<pair<ET_Sound, ET_Sound>, int64_t> m_mapSoundPairToId;
+        map<int64_t, int64_t> m_mapSoundPairIdToCount;
+        set<tuple<int64_t, ET_Sound, ET_Sound>> m_setNewSoundPairs;
         CEString m_sCurrentSequence;
     };
 }
